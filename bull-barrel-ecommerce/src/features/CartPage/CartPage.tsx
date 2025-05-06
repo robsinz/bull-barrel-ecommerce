5e530c5f3cf8273c9cf64ad4447a9507c8e8ab0d
Rob Sinz <robsin@gmail.com>
2025-05-05 17:32:20 -0700

    completed css for CartPage component

--- bull-barrel-ecommerce/src/features/CartPage/CartPage.tsx
+++ bull-barrel-ecommerce/src/features/CartPage/CartPage.tsx
@@ -11,8 +11,8 @@ const CartPage = () => {
       productId: 't-shirt',
       name: 'bull n barrel t shirt',
       price: 29.99,
-      color: 'white',
-      size: 'm',
+      color: 'White',
+      size: 'M',
       quantity: 2,
       image: tshirtFrontImage,
     },
@@ -20,29 +20,46 @@ const CartPage = () => {
       productId: 't-shirt',
       name: 'bull n barrel t shirt',
       price: 29.99,
-      color: 'black',
-      size: 'l',
-      quantity: 4,
+      color: 'Black',
+      size: 'L',
+      quantity: 1,
+      image: tshirtFrontImage,
+    },
+    {
+      productId: 't-shirt',
+      name: 'bull n barrel t shirt',
+      price: 29.99,
+      color: 'Black',
+      size: 'L',
+      quantity: 1,
+      image: tshirtFrontImage,
+    },
+    {
+      productId: 't-shirt',
+      name: 'bull n barrel t shirt',
+      price: 29.99,
+      color: 'Black',
+      size: 'L',
+      quantity: 1,
       image: tshirtFrontImage,
     },
   ];
 
   return (
-    <div className="container">
+    <div className="main-cart-container">
       <h2 className="cart-title">Shopping Cart</h2>
       <div className="item-container">
         {testCart.length > 0 ? (
           <div className="cart-items">
             {testCart.map((item) => (
               <div key={item.productId} className="cart-item-grid">
-                <div className="product-info">
-                  <img src={item.image} alt={item.name} className="cart-item-image" />
-                  <div className="product-details">
-                    <p className="item-name">{item.name}</p>
-                    {item.size && <p className="item-variant">Size: {item.size}</p>}
-                    {item.color && <p className="item-variant">Color: {item.color}</p>}
-                  </div>
+                <img src={item.image} alt={item.name} className="cart-item-image" />
+                <div className="product-details">
+                  <p className="item-name">{item.name}</p>
+                  {item.color && <p className="item-variant">Color: {item.color}</p>}
+                  {item.size && <p className="item-variant">Size: {item.size}</p>}
                 </div>
+
                 <div className="quantity-controls">
                   <button
                     onClick={() =>
@@ -71,10 +88,9 @@ const CartPage = () => {
                     +
                   </button>
                 </div>
-                <div className="price-container">
+
+                <div className="price-remove-container">
                   <p className="item-total">${(item.price * item.quantity).toFixed(2)}</p>
-                </div>
-                <div className="remove-controls">
                   <button
                     className="remove-button"
                     onClick={() =>
@@ -98,11 +114,9 @@ const CartPage = () => {
           <p className="subtotal-label">Subtotal:</p>
           <p className="subtotal-amount">${getCartTotal().toFixed(2)}</p>
         </div>
-        <div className="checkout">
-          <button className="checkout-button" onClick={() => console.log('you checked out')}>
-            CHECKOUT
-          </button>
-        </div>
+        <button className="checkout-button" onClick={() => console.log('you checked out')}>
+          CHECKOUT
+        </button>
       </div>
     </div>
   );
